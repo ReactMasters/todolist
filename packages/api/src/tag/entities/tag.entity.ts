67f167e68afda4cb453aa85739c65669e0c3a0a6
Dasol Jung <56aiden90@gmail.com>
2022-02-18 19:16:06 -0500

    chore: remove unique index from the User property

--- packages/api/src/tag/entities/tag.entity.ts
+++ packages/api/src/tag/entities/tag.entity.ts
@@ -1,9 +1,9 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql'
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import * as mongoose from 'mongoose'
 import { Document } from 'mongoose'
 import { Auditable } from 'src/types/auditable/auditable.entity'
 import { User } from 'src/user/entities/user.entity'
-import * as mongoose from 'mongoose'
 
 export type TagDocument = Tag & Document
 
@@ -19,7 +19,7 @@ export class Tag extends Auditable {
   name: string
 
   @Field(() => User)
-  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', unique: true })
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
   owner: User
 }
 
