f1577231429a5e8caeb60e64d874d14053a5aee1
오제관 <emperorvv@gmail.com>
2022-03-13 01:42:54 +0900

    feat: Add index page mock

---
+++ packages/web/pages/login/index.page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './index.module.scss'
+type Props = {}
+
+const LoginPage = (props: Props) => {
+  return <div className={styles.wrapper}>LoginPage</div>
+}
+
+export default LoginPage
