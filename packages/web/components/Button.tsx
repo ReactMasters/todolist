b2a41741e7efbdf8c7ac787f7c3b5eb1761c001e
오제관 <emperorvv@gmail.com>
2022-03-19 22:13:10 +0900

    feat: Add default button

---
+++ packages/web/components/Button.tsx
@@ -0,0 +1,25 @@
+import classNames from 'classnames'
+import { ButtonHTMLAttributes, FC } from 'react'
+import styles from './Button.module.scss'
+interface ButtonProps
+  extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'type'> {
+  type?: 'default' | 'weak' | 'primary' | 'outlined' | 'transparent'
+  htmlType?: ButtonHTMLAttributes<HTMLButtonElement>['type']
+  size?: 'small' | 'medium' | 'large'
+}
+
+const Button: FC<ButtonProps> = ({
+  type,
+  className,
+  size = 'medium',
+  ...props
+}) => {
+  return (
+    <button
+      className={classNames(styles.button, type, size, className)}
+      {...props}
+    />
+  )
+}
+
+export default Button
