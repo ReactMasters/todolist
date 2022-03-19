230fe9f05fddb5a1e4057ab371bfc0207db6db4c
오제관 <emperorvv@gmail.com>
2022-03-19 22:36:17 +0900

    feat: Add mock login page

--- packages/web/pages/login/index.page.tsx
+++ packages/web/pages/login/index.page.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import { gql } from '@apollo/client'
+import { APP_TITLE } from '@web/lib/constant'
+import { useRouter } from 'next/router'
+
 import styles from './index.module.scss'
+import { useLoginPageQuery } from './index.page.generated'
+
 type Props = {}
 
+export const query = gql`
+  query LoginPage {
+    me {
+      __typename
+      ... on MeError {
+        message
+      }
+      ... on MeSuccess {
+        user {
+          id
+          email
+        }
+      }
+    }
+  }
+`
+
 const LoginPage = (props: Props) => {
-  return <div className={styles.wrapper}>LoginPage</div>
+  const router = useRouter()
+  const { data } = useLoginPageQuery()
+
+  if (data?.me?.__typename === 'MeSuccess') {
+    // 이미 로그인됨
+    router.replace('/')
+    return null
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>{APP_TITLE}</h1>
+      <input type="text" />
+      <input type="text" />
+      <button>로그인</button>
+      <button>회원가입</button>
+    </div>
+  )
 }
 
 export default LoginPage
