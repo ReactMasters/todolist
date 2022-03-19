40a79eb80d948bee04bdd3de076212974a6efea3
오제관 <emperorvv@gmail.com>
2022-03-20 01:52:35 +0900

    feat: Add login mutation and handling

--- packages/web/pages/login/components/LoginForm.tsx
+++ packages/web/pages/login/components/LoginForm.tsx
@@ -1,20 +1,63 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { FormEventHandler, useCallback } from 'react'
-import Email from './email'
+import Cookies from 'js-cookie'
+import { gql, useReactiveVar } from '@apollo/client'
+import { MESSAGES, TOKEN_KEY } from '@web/lib/constant'
+
+import { emailVar, loginLoadingVar, passwordVar } from '../index.state'
+import Email from './Email'
+import { useLoginMutation } from './LoginForm.generated'
 import Password from './Password'
+import { useRouter } from 'next/router'
 
-type Props = {}
+export const query = gql`
+  mutation Login($loginInput: LoginInput!) {
+    login(loginInput: $loginInput) {
+      ... on LoginSuccess {
+        token
+      }
+      ... on LoginError {
+        message
+      }
+    }
+  }
+`
 
-const LoginForm = (props: Props) => {
-  const handleSubmit = useCallback<FormEventHandler>((e) => {
+const LoginForm = () => {
+  const router = useRouter()
+  const [login] = useLoginMutation()
+  const loading = useReactiveVar(loginLoadingVar)
+  const handleSubmit = useCallback<FormEventHandler>(async (e) => {
     e.preventDefault()
-    // todo login mutation and handling
+    if (loginLoadingVar()) return alert(MESSAGES.MUTATION_LOADING)
+    loginLoadingVar(true)
+    const email = emailVar()
+    const password = passwordVar()
+    const { data } = await login({
+      variables: {
+        loginInput: {
+          email,
+          password,
+        },
+      },
+    })
+
+    if (data.login.__typename === 'LoginError') {
+      loginLoadingVar(false)
+      return alert(data.login.message)
+    }
+
+    Cookies.set(TOKEN_KEY, data.login.token)
+    router.replace('/')
   }, [])
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <Email />
         <Password />
-        <button type="submit">로그인</button>
+        <button type="submit" disabled={loading}>
+          로그인
+        </button>
       </form>
       <button type="button">회원가입</button>
     </div>
