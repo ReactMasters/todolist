1c1503a9caec7b287b6f53bf614d2c2b3ed7ad5b
Hyunjoo <hyunjoopark0208@gmail.com>
2022-03-19 18:12:30 +0100

    feat: Create basic layout

--- packages/web/components/TodoItem.tsx
+++ packages/web/components/TodoItem.tsx
@@ -1,3 +1,5 @@
+import { CalendarToday, LocalOfferRounded } from '@mui/icons-material'
+import { Checkbox } from 'antd'
 import styles from './TodoItem.module.scss'
 
 interface Props {
@@ -5,7 +7,24 @@ interface Props {
 }
 
 const TodoItem: React.FC<Props> = ({ name }) => {
-  return <div className={styles.todoItem}>{name}</div>
+  return (
+    <div className={styles.todoItem}>
+      <Checkbox />
+      <div>
+        <div>{name}</div>
+        <div>
+          <div>
+            <CalendarToday />
+            Tomorrow
+          </div>
+          <div>
+            <LocalOfferRounded />
+            Tag1
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default TodoItem
