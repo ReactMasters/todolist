7792b99ebb3676992ed2c6bd06d47dff3af6aa36
오제관 <emperorvv@gmail.com>
2022-03-19 22:35:36 +0900

    feat: Add ui page to look up uis

---
+++ packages/web/pages/ui.page.tsx
@@ -0,0 +1,14 @@
+import Button from '@web/components/Button'
+
+const UIPage = () => {
+  return (
+    <div style={{ padding: 10 }}>
+      <Button>default</Button>
+      <Button size="small">size:small</Button>
+      <Button size="medium">size:medium</Button>
+      <Button size="large">size:large</Button>
+    </div>
+  )
+}
+
+export default UIPage
