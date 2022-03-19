1ff32f112b8edc546311e1549de58c23aaa413a7
56aiden90 <56aiden90@gmail.com>
2022-03-19 09:49:21 -0400

    feat: Add a theme provider

--- packages/web/pages/_app.page.tsx
+++ packages/web/pages/_app.page.tsx
@@ -4,13 +4,21 @@ import { AppProps } from 'next/app'
 
 import { ApolloProvider } from '@apollo/client'
 import { useApollo } from '@web/lib/apollo/client'
+import { LocalizationProvider } from '@mui/lab'
+import DateAdapter from '@mui/lab/AdapterDayjs'
+import { createTheme, ThemeProvider } from '@mui/material'
 
 export default function App({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps.initialApolloState)
+  const defaultTheme = createTheme()
 
   return (
     <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
+      <LocalizationProvider dateAdapter={DateAdapter}>
+        <ThemeProvider theme={defaultTheme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </LocalizationProvider>
     </ApolloProvider>
   )
 }
