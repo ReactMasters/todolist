8007adb7e98f0b66724c47429e0a608f25cffa0b
56aiden90 <56aiden90@gmail.com>
2022-03-25 18:12:51 -0400

    feat: Add findTodoList function to the todo list service

--- packages/api/src/todo-list/todo-list.service.ts
+++ packages/api/src/todo-list/todo-list.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 import { Model } from 'mongoose'
 import { CreateTodoListInput } from './dto/create-todo-list.input'
+import { FindTodoListInput } from './dto/find-todo-list.input'
 import { TodoList, TodoListDocument } from './entities/todo-list.entity'
 
 @Injectable()
@@ -14,5 +15,10 @@ export class TodoListService {
   async createTodoList(createTodoListInput: CreateTodoListInput) {
     const { name } = createTodoListInput
     return await new this.TodoListModel({ name }).save()
+  }
+
+  async findTodoList(findTodoListInput: FindTodoListInput) {
+    const { id } = findTodoListInput
+    return await this.TodoListModel.findById(id)
   }
 }
