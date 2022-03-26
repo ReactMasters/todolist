f7aa8d718b1c08ae9485b95f74ad5155c9db2171
56aiden90 <56aiden90@gmail.com>
2022-03-26 11:16:06 -0400

    feat: Add TodoList and User modules and services to TodoItem module

--- packages/api/src/todo-item/todo-item.module.ts
+++ packages/api/src/todo-item/todo-item.module.ts
@@ -1,5 +1,13 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
+import {
+  TodoList,
+  TodoListSchema,
+} from 'src/todo-list/entities/todo-list.entity'
+import { TodoListService } from 'src/todo-list/todo-list.service'
+import { User, UserSchema } from 'src/user/entities/user.entity'
+import { UserModule } from 'src/user/user.module'
+import { UserService } from 'src/user/user.service'
 import { TodoItem, TodoItemSchema } from './entities/todo-item.entity'
 import { TodoItemResolver } from './todo-item.resolver'
 import { TodoItemService } from './todo-item.service'
@@ -9,7 +17,12 @@ import { TodoItemService } from './todo-item.service'
     MongooseModule.forFeature([
       { name: TodoItem.name, schema: TodoItemSchema },
     ]),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([
+      { name: TodoList.name, schema: TodoListSchema },
+    ]),
+    UserModule,
   ],
-  providers: [TodoItemResolver, TodoItemService],
+  providers: [TodoItemResolver, TodoItemService, TodoListService, UserService],
 })
 export class TodoItemModule {}
