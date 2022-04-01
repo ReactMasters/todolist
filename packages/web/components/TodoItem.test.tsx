dd5f9d46509ab6a0b374caf79eb5152a9ad04f16
ipf-yychoi <yychoi@iportfoilo.co.kr>
2022-04-01 21:20:02 +0900

    test: Add titles of test cases for TodoItem

---
+++ packages/web/components/TodoItem.test.tsx
@@ -0,0 +1,13 @@
+import TodoItem from './TodoItem'
+
+describe('tests TodoItem component', () => {
+  it('name이 보인다.', () => {})
+  it('checkbox 상태가 표시된다.', () => {})
+  it('만료일이 있을 경우, 만료일이 MMM D, YYYY 형식으로 표시된다.', () => {})
+  it('만료됐을 경우, 아이콘과 날짜 표시가 빨간색으로 표시된다.', () => {})
+  it('TagList의 태그명이 모두 표시된다.', () => {})
+  it('체크되었을 경우, Strikethrough 스타일을 적용한다.', () => {})
+  it('체크 해제되었을 경우, Strikethrough 스타일을 해제한다.', () => {})
+  it('만료일 정보만 없을 경우, name과 TagList만 표시된다.', () => {})
+  it('TagList 정보만 없을 경우, name과 만료일만 표시된다.', () => {})
+})
