e9c5cc161cd67078d3ef67a10128850889568eee
56aiden90 <56aiden90@gmail.com>
2022-04-13 12:35:33 -0400

    feat: Add the TodoList module and service to the User module

--- packages/api/src/user/user.module.ts
+++ packages/api/src/user/user.module.ts
@@ -1,5 +1,10 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
+import {
+  TodoList,
+  TodoListSchema,
+} from 'src/todo-list/entities/todo-list.entity'
+import { TodoListService } from 'src/todo-list/todo-list.service'
 import { User, UserSchema } from './entities/user.entity'
 import { UserResolver } from './user.resolver'
 import { UserService } from './user.service'
@@ -7,7 +12,10 @@ import { UserService } from './user.service'
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([
+      { name: TodoList.name, schema: TodoListSchema },
+    ]),
   ],
-  providers: [UserResolver, UserService],
+  providers: [UserResolver, UserService, TodoListService],
 })
 export class UserModule {}
