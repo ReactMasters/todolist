3a6fdd0e1fbe121991bf6bccfb8ee4fa371dd24e
ipf-yychoi <yychoi@iportfoilo.co.kr>
2022-04-23 15:49:22 +0900

    feat: Add Completed Button to TodoList

--- packages/web/components/TodoList/TodoList.tsx
+++ packages/web/components/TodoList/TodoList.tsx
@@ -1,4 +1,6 @@
+import { DownOutlined } from '@ant-design/icons'
 import { gql, useQuery } from '@apollo/client'
+import { Button } from 'antd'
 import React from 'react'
 import TodoItem from '../TodoItem/TodoItem'
 import { FindTodoListDocument } from './TodoList.generated'
@@ -40,7 +42,15 @@ const TodoList = ({ todoListId }: Props) => {
   const todos = (data?.findTodoList.todoList.todos ?? []).map((todo) => {
     return <TodoItem key={todo.id} todo={todo} />
   })
-  return <div>{todos}</div>
+
+  return (
+    <div>
+      {todos}
+      <Button type="primary" icon={<DownOutlined />}>
+        Completed
+      </Button>
+    </div>
+  )
 }
 
 export default TodoList
