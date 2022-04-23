a125fd2b0dee6d76d93b4b9e336426a869a5d893
JeGwan <emperorvv@gmail.com>
2022-04-24 00:06:54 +0900

    refactor: Move resetStore position

--- packages/web/components/AuthForm/index.tsx
+++ packages/web/components/AuthForm/index.tsx
@@ -18,6 +18,7 @@ import { LoginDocument, SignUpDocument } from './index.generated'
 import { emailVar, loginLoadingVar, passwordVar } from './index.state'
 import Password from './Password'
 import Link from 'next/link'
+import { LoginInput } from '@web/lib/graphql/types'
 
 const Login = gql`
   mutation Login($loginInput: LoginInput!) {
@@ -67,40 +68,32 @@ const AuthForm = ({ type }: Props) => {
     loginLoadingVar(true)
 
     let token = ''
+
+    const inputs: LoginInput = {
+      email: emailVar(),
+      password: passwordVar(),
+    }
     if (type === 'login') {
-      const { data } = await login({
-        variables: {
-          loginInput: {
-            email: emailVar(),
-            password: passwordVar(),
-          },
-        },
-      })
+      const { data } = await login({ variables: { loginInput: inputs } })
       if (data.login.__typename === 'LoginError') {
         loginLoadingVar(false)
         return alert(data.login.message)
       }
-      client.resetStore()
       token = data.login.token
     } else {
-      const { data } = await signup({
-        variables: {
-          signUpInput: {
-            email: emailVar(),
-            password: passwordVar(),
-          },
-        },
-      })
+      const { data } = await signup({ variables: { signUpInput: inputs } })
       if (data.createUser.__typename === 'CreateUserError') {
         loginLoadingVar(false)
         return alert(data.createUser.message)
       }
       token = data.createUser.token
     }
+
     Cookies.set(TOKEN_KEY, token)
     emailVar('')
     passwordVar('')
     loginLoadingVar(false)
+    client.resetStore()
     router.replace('/')
   }, [])
 
