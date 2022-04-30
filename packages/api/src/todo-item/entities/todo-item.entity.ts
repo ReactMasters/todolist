378073839230bb1c9bc72e5ad6a81f237838b890
JeGwan <emperorvv@gmail.com>
2022-05-01 01:29:21 +0900

    feat: Add todoListId to todoItem

--- packages/api/src/todo-item/entities/todo-item.entity.ts
+++ packages/api/src/todo-item/entities/todo-item.entity.ts
@@ -1,14 +1,16 @@
 import { Field, ObjectType } from '@nestjs/graphql'
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+
 import * as mongoose from 'mongoose'
 import { BaseEntity } from 'src/base/base.entity'
 import { Tag } from 'src/tag/entities/tag.entity'
 import { TodoStatus } from 'src/todo-item/dto/todo-status.enum'
+import { TodoList } from 'src/todo-list/entities/todo-list.entity'
 
 export type TodoDocument = TodoItem & mongoose.Document
 
 @ObjectType()
-@Schema()
+@Schema({ toObject: { virtuals: true }, timestamps: true })
 export class TodoItem extends BaseEntity {
   @Field()
   @Prop()
@@ -27,6 +29,12 @@ export class TodoItem extends BaseEntity {
   @Field({ nullable: true })
   @Prop({ type: Date })
   dueDateTime: Date | null
+
+  @Prop({ type: { type: mongoose.Schema.Types.ObjectId, ref: 'TodoList' } })
+  todoListId: string
+
+  @Field(() => TodoList)
+  todoList: TodoList
 }
 
 export const TodoItemSchema = SchemaFactory.createForClass(TodoItem)
