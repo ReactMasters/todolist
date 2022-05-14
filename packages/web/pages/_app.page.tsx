9e633f470ddaf85797d75eb5af6288191dac4469
오제관 <emperorvv@gmail.com>
2022-05-14 23:27:50 +0900

    fix: Resolve meta tag warning

--- packages/web/pages/_app.page.tsx
+++ packages/web/pages/_app.page.tsx
@@ -2,12 +2,13 @@ import '@web/styles/global.scss'
 import 'antd/dist/antd.css'
 
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 
 import { ApolloProvider } from '@apollo/client'
-import { useApollo } from '@web/lib/apollo/client'
 import { LocalizationProvider } from '@mui/lab'
 import DateAdapter from '@mui/lab/AdapterDayjs'
 import { createTheme, ThemeProvider } from '@mui/material'
+import { useApollo } from '@web/lib/apollo/client'
 
 export default function App({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps.initialApolloState)
@@ -17,6 +18,12 @@ export default function App({ Component, pageProps }: AppProps) {
     <ApolloProvider client={apolloClient}>
       <LocalizationProvider dateAdapter={DateAdapter}>
         <ThemeProvider theme={defaultTheme}>
+          <Head>
+            <meta
+              name="viewport"
+              content="initial-scale=1, width=device-width"
+            />
+          </Head>
           <Component {...pageProps} />
         </ThemeProvider>
       </LocalizationProvider>
