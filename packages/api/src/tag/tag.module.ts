1bdde6278ba0425418a8feb8e50cf509d2287926
오제관 <emperorvv@gmail.com>
2022-05-28 22:16:55 +0900

    refactor: Format code

--- packages/api/src/tag/tag.module.ts
+++ packages/api/src/tag/tag.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common'
-import { MongooseModule } from '@nestjs/mongoose';
-import { Tag, TagSchema } from './entities/tag.entity';
-import { TagResolver } from './tag.resolver';
-import { TagService } from './tag.service';
+import { MongooseModule } from '@nestjs/mongoose'
+
+import { Tag, TagSchema } from './entities/tag.entity'
+import { TagResolver } from './tag.resolver'
+import { TagService } from './tag.service'
 
 @Module({
-    imports: [
-        MongooseModule.forFeature([{ name: Tag.name, schema: TagSchema }]),
-    ],
-    providers: [TagResolver, TagService],
+  imports: [MongooseModule.forFeature([{ name: Tag.name, schema: TagSchema }])],
+  providers: [TagResolver, TagService],
 })
-export class TagModule { }
+export class TagModule {}
--- packages/api/src/user/entities/user.entity.ts
+++ packages/api/src/user/entities/user.entity.ts
@@ -1,5 +1,6 @@
 import { Field, ObjectType } from '@nestjs/graphql'
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+
 import { Document } from 'mongoose'
 import { BaseEntity } from 'src/base/base.entity'
 
